23ee2791b6dd0ca81a670b7eae065d186ec588a9
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-11-09 13:10:30 -0500

    added character getStats and getInventory to check properties of character

--- __tests__/Character.test.js
+++ __tests__/Character.test.js
@@ -3,11 +3,11 @@ const Character = require("../lib/Character");
 const Potion = require("../lib/Potion");
 jest.mock("../lib/Potion.js");
 
-console.log(new Potion());
+// console.log(new Potion());
 
 test("Creates a Character", () => {
   const character = new Character("GeeTheGladiator");
-  console.log("Character Name", character.name.length);
+  //   console.log("Character Name", character.name.length);
   expect(character.name).toEqual(expect.any(String));
   expect(character.name.length).toBeGreaterThan(5);
 });
@@ -21,10 +21,45 @@ test("Character abilities are default, h:40, aglt:1, s:1, a:2", () => {
   expect(character.attack).toEqual(2);
 });
 
-test("Character inventory", () => {
+test("Character has inventory", () => {
   const character = new Character("GeeTheGladiator");
 
   expect(character.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("Character has potions in inventory, inventory is 2", () => {
+  const character = new Character("GeeTheGladiator");
+
+  const characterInventory = character.inventory;
+  const healthPotion = characterInventory[0];
+  const randomPotion = characterInventory[1];
+  //   console.log("Health Potion", healthPotion);
+  //   console.log("Total inventory: ", characterInventory);
+
+  expect(healthPotion.name).toBe("health");
+  expect(randomPotion.name).toEqual(expect.any(String));
+  expect(characterInventory.length).toBe(2);
+});
+
+test("Character has stats as an object", () => {
+  const character = new Character("GeeTheGladiator");
+
+  expect(character.getStats()).toHaveProperty("potions");
+  expect(character.getStats()).toHaveProperty("health");
+  expect(character.getStats()).toHaveProperty("strength");
+  expect(character.getStats()).toHaveProperty("agility");
+  expect(character.getStats()).toHaveProperty("attack");
+  expect(character.getStats()).toHaveProperty("defence");
+});
+
+test("Getting inventory from character or return false", () => {
+  const character = new Character("GeeTheGladiator");
+
+  expect(character.getInventory()).toEqual(expect.any(Array));
+  console.log(character.getInventory());
+  character.inventory = [];
+
+  expect(character.getInventory()).toEqual([]);
 });
