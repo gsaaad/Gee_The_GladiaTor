984cf23728d68576cbaa4fd3d1aa540849809e9b
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-11-09 12:31:22 -0500

    added mock potion embedded in Character class, each character will have inventory with health potion and 1 random potion

--- __tests__/Character.test.js
+++ __tests__/Character.test.js
@@ -1,4 +1,9 @@
 const Character = require("../lib/Character");
+// const Potion = require("../lib/__mocks__/Potion");
+const Potion = require("../lib/Potion");
+jest.mock("../lib/Potion.js");
+
+console.log(new Potion());
 
 test("Creates a Character", () => {
   const character = new Character("GeeTheGladiator");
@@ -6,6 +11,7 @@ test("Creates a Character", () => {
   expect(character.name).toEqual(expect.any(String));
   expect(character.name.length).toBeGreaterThan(5);
 });
+
 test("Character abilities are default, h:40, aglt:1, s:1, a:2", () => {
   const character = new Character("GeeTheGladiator");
 
@@ -13,4 +19,12 @@ test("Character abilities are default, h:40, aglt:1, s:1, a:2", () => {
   expect(character.strength).toEqual(1);
   expect(character.health).toEqual(40);
   expect(character.attack).toEqual(2);
+});
+
+test("Character inventory", () => {
+  const character = new Character("GeeTheGladiator");
+
+  expect(character.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
 });
